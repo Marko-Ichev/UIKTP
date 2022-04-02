1404702fdda0a247cdf863c8790d467cb6c046fc
cuneythuseini <63378887+cuneythuseini@users.noreply.github.com>
2022-04-02 17:24:51 +0200

    Added Spring Boot web application

---
+++ src/test/java/team20/mk/ukim/finki/skit/SKitApplicationTests.java
@@ -0,0 +1,13 @@
+package team20.mk.ukim.finki.skit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

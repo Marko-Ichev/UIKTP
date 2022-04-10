fcb2febe318f3ad945af87ecd9c6ff5a728f3a7f
Filip Petrovski <filip.petrovski.1@students.finki.ukim.mk>
2022-04-10 19:58:11 +0200

    Update in Order class for database name and change active profiles.

--- src/main/java/team20/mk/ukim/finki/skit/SKitApplication.java
+++ src/main/java/team20/mk/ukim/finki/skit/SKitApplication.java
@@ -2,8 +2,9 @@ package team20.mk.ukim.finki.skit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SKitApplication {
 
 	public static void main(String[] args) {
